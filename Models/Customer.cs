a7ac4f834f9af1b9ad867eab900da01dc4dc9946
Jamin Ewell <jamin.ewell@charlie24.com>
2023-07-03 22:53:13 +0300

    controller

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,16 @@
-﻿namespace CustomerAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerAPI.Models
 {
+    [Table("Customer")]
     public class Customer
     {
+        [Key]
+        public int CustId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
     }
 }
